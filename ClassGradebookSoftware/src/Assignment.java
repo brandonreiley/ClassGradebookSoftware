0fa41d7bad3df4605cc56f5600dcaf894cf44d2b
Brandon Reiley <breiley@sdsu.edu>
2021-03-22 08:41:32 -1000

    initial commits, uploading project to repo

---
+++ ClassGradebookSoftware/src/Assignment.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Assignment {
+	private String type;
+	private String description;
+	private int ID;
+	private double percentage;
+
+	public Assignment(String type, String description, int ID, double percentage) {
+		this.type = type;
+		this.description = description;
+		this.ID = ID;
+		this.percentage = percentage;
+	}
+	
+	public static void main(String[] args) {
+		ArrayList<Assignment> list = new ArrayList<Assignment>();
+		
+		
+	}
+}
